8225ccea786ebd04938e3c819593654b1dde0075
Matthew Albert <mmalbert02@gmail.com>
2021-02-26 21:02:41 -0800

    css / starter html

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="w3-content">
+
+      {/* <!-- Header --> */}
+      <header class="w3-panel w3-center w3-opacity">
+        <h1 class="w3-xlarge">PHOTOGRAPHER</h1>
+        <h1>John Doe</h1>
+        
+        <div class="w3-padding-32">
+          <div class="w3-bar w3-border">
+            <p  class="w3-bar-item w3-button">Home</p>
+            <p  class="w3-bar-item w3-button w3-light-grey">Portfolio</p>
+            <p class="w3-bar-item w3-button">Contact</p>
+            <p class="w3-bar-item w3-button w3-hide-small">Weddings</p>
+          </div>
+        </div>
       </header>
-    </div>
+
+      {/* <!-- Photo Grid --> */}
+      <div class="w3-row-padding w3-grayscale">
+        <div class="w3-half">
+          
+        </div>
+
+        <div class="w3-half">
+          
+        </div>
+      </div>
+        
+{/* <!-- End Page Content --> */}
+</div>
   );
+
 }
 
 export default App;
