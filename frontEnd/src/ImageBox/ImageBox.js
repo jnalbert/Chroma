ccd9d67c7d6b0707654070a75692854cae9448d9
Justin Albert <Jnalbert879@gmail.com>
2021-03-05 12:37:40 -0800

    fixed some css

--- frontEnd/src/ImageBox/ImageBox.js
+++ frontEnd/src/ImageBox/ImageBox.js
@@ -2,19 +2,24 @@ import '../App/App.css'
 import './ImageBox.css'
 import React from 'react'
 import frame from './frame.png'
-
+// w3-half
+// style={{display: "inline-flex", transform: `translate(43px, 0%)`}}
 function ImageBox ({imageUpload, imageDownload}) {
     return (
         <div className="w3-row-padding w3-center" style={{marginBottom: "128px", transform: `translateY(-80px)`}}>
-            <div className="w3-half" style={{display: "inline-flex", transform: `translate(43px, 0%)`}}>
-            {imageUpload === ""
-                ? <img src={frame} alt="frame" style={{width: "90%"}} />
-                : <img className="image-scale" src={imageUpload} alt="" width="977" height="682"/>
-                }
-            {imageDownload === ""
-                ? <img src={frame} alt="frame" style={{width: "90%"}}/>
-                : <img className="image-scale" src={imageDownload} alt="" width="977" height="682"/>
-                }
+            <div className="w3-half" >
+                <div className="img-display1">
+                    {imageUpload === ""
+                        ? <img className="img-frame1" src={frame} alt="frame" style={{width: "90%"}} />
+                        : <img className="image-scale" src={imageUpload} alt="" width="977" height="682"/>
+                        }
+                </div>
+                <div className="img-display2">
+                    {imageDownload === ""
+                        ? <img className="img-frame2" src={frame} alt="frame" style={{width: "90%"}}/>
+                        : <img className="image-scale" src={imageDownload} alt="" width="977" height="682"/>
+                        }
+                </div>
             </div>
         </div>
     )
